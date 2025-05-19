e5f37bdc53a0fdfa6887f4ca498e2976024447d5
lhbccf <luis.f.work98@gmail.com>
2025-05-19 19:51:02 +0100

    Upload image now works for large data files

--- CNTF_Client/src/main/java/CN_Client.java
+++ CNTF_Client/src/main/java/CN_Client.java
@@ -1,10 +1,12 @@
-import client.ObserverUploadImage;
+
 import com.google.cloud.storage.*;
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import servicestubs.*;
 
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -70,7 +72,7 @@ public class CN_Client {
 
         System.out.print("Enter image path to upload: ");
         String imagePath = scanner.nextLine().trim();
-        String imageName = imagePath.toString();
+        String imageName = imagePath;
 
         if(!imagePath.contains("./")){
             imagePath = "./" + imagePath;
@@ -80,18 +82,41 @@ public class CN_Client {
 
         Path uploadPath = Paths.get(imagePath);
 
-        try {
-            ImageFile image = ImageFile.newBuilder()
-                    .setImageType(Files.probeContentType(uploadPath))
-                    .setImageName(imageName)
-                    .setImageData(ByteString.copyFrom(Files.readAllBytes(uploadPath)))
-                    .build();
+        //File big
+        StreamObserver<ImageBlock> streamBlocks = noBlockStub.uploadImage(
+                new StreamObserver<ReturnFile>() {
+                    @Override
+                    public void onNext(ReturnFile value) {
+                        System.out.println("Block sent");
+                    }
+                    @Override
+                    public void onError(Throwable t) {
+                        System.out.println("Error on upload");
+                    }
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("Image sent successfully");
+                    }
+                }
+        );
 
-            ObserverUploadImage imageObserver = new ObserverUploadImage();
-            noBlockStub.uploadImage(image, imageObserver);
-
+        // When Blob size is big or unknown use the blob's channel reader.
+        try (FileInputStream inStream = new FileInputStream(uploadPath.toFile())){
+            byte[] buffer = new byte[8192];
+            while(inStream.read(buffer) > 0){
+                streamBlocks.onNext(
+                        ImageBlock.newBuilder()
+                                .setImageName(imageName)
+                                .setImageType(Files.probeContentType(uploadPath))
+                                .setDataBlock(ByteString.copyFrom(buffer))
+                                .build()
+                );
+                buffer = new byte[8192];
+            }
+            streamBlocks.onCompleted();
         } catch (Exception e) {
-            System.out.println("File not found");
+            streamBlocks.onError(e);
+            System.out.println(e.getMessage());
         }
 
     }
--- CNTF_Server/src/main/java/server/CNTFService.java
+++ CNTF_Server/src/main/java/server/CNTFService.java
@@ -1,8 +1,12 @@
 package server;
 
+import com.google.cloud.ByteArray;
 import com.google.cloud.storage.*;
 import io.grpc.stub.StreamObserver;
 import servicestubs.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public class CNTFService extends cn2425tfGrpc.cn2425tfImplBase {
 
@@ -11,33 +15,53 @@ public class CNTFService extends cn2425tfGrpc.cn2425tfImplBase {
     }
 
     @Override
-    public void uploadImage(ImageFile image, StreamObserver<ReturnFile> responseObserver){
+    public StreamObserver<ImageBlock> uploadImage(StreamObserver<ReturnFile> responseObserver){
         StorageOptions storageOptions = StorageOptions.getDefaultInstance();
         Storage storage = storageOptions.getService();
-
-        BlobId blobId = BlobId.of("cn2425tf_g06", image.getImageName());
-        Blob blob = storage.get(blobId);
-
-        if (blob == null) {
-            try {
-                BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
-                        .setContentType(image.getImageType())
-                        .build();
-                blob = storage.create(blobInfo, image.getImageData().toByteArray());
-
-                ReturnFile response = ReturnFile.newBuilder()
-                        .setFileId(blob.asBlobInfo().getBlobId().toString())
-                        .build();
-
-                responseObserver.onNext(response);
-                responseObserver.onCompleted();
-            } catch(Exception e){
-                System.out.println("File not found");
-                responseObserver.onError(e);
+        ByteArrayOutputStream bytesReceived = new ByteArrayOutputStream();
+        final String[] imageName = {""};
+        final String[] imageType = {""};
+        return new StreamObserver<ImageBlock>() {
+            @Override
+            public void onNext(ImageBlock value) {
+                imageName[0] = value.getImageName();
+                imageType[0] = value.getImageType();
+                bytesReceived.writeBytes(value.getDataBlock().toByteArray());
             }
-        } else {
-            System.out.println("Blob name already exists");
-        }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                bytesReceived.toByteArray();
+                BlobId blobId = BlobId.of("cn2425tf_g06", imageName[0]);
+                Blob blob = storage.get(blobId);
+
+                if (blob == null) {
+                    try {
+                        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+                                .setContentType(imageType[0])
+                                .build();
+                        blob = storage.create(blobInfo, bytesReceived.toByteArray());
+
+                        ReturnFile response = ReturnFile.newBuilder()
+                                .setRequestId(blob.asBlobInfo().getBlobId().toString())
+                                .build();
+
+                        responseObserver.onNext(response);
+                        responseObserver.onCompleted();
+                    } catch(Exception e){
+                        System.out.println("File not found");
+                        responseObserver.onError(e);
+                    }
+                } else {
+                    System.out.println("Blob name already exists");
+                }
+            }
+        };
     }
 
 }
